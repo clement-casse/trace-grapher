23d0a26964294f91087e36e93b7e2c4e878d770e
Clément Cassé <casse.clement@outlook.com>
2019-11-28 23:23:54 +0100

    Add ECK to have an ES backend for Jaeger

--- provision-k8s/vars.mk
+++ provision-k8s/vars.mk
@@ -8,6 +8,7 @@ STRIMZI_VERSION := 0.14.0
 STRIMZI_OPERATOR_NS := kafka-operator
 PROM_OPERATOR_NS := prometheus-operator
 JAEGER_OPERATOR_NS := jaeger-operator
+ECK_VERSION := 1.0.0-beta1
 
 # Linkerd2 Version Number as written in the Github Release page: https://github.com/linkerd/linkerd2/releases/
 LINKERD2_VERSION := stable-2.6.0
