8d34107ed25d50fecc708edcd5167d7edf09404a
Clément Cassé <casse.clement@outlook.com>
2019-12-11 17:23:34 +0100

    Did not managed to have linkerd working -> go istio
    Removing Linkerd and nginx-ingress from provisioning
    Adding Isitio and breaking all the things, all the things but neo4j

--- provision-k8s/vars.mk
+++ provision-k8s/vars.mk
@@ -14,5 +14,4 @@ PROM_OPERATOR_NS := prometheus-operator
 JAEGER_OPERATOR_NS := jaeger-operator
 ECK_VERSION := 1.0.0-beta1
 
-# Linkerd2 Version Number as written in the Github Release page: https://github.com/linkerd/linkerd2/releases/
-LINKERD2_VERSION := stable-2.6.0
+ISTIO_VERSION := 1.4.2
