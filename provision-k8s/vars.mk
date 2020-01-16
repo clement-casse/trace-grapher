03a60969a0ee8f42fa1e93e76a50da50522cc03d
Clément Cassé <casse.clement@outlook.com>
2020-01-16 11:08:56 +0100

    Refactor K8S deployment to use different NS

--- provision-k8s/vars.mk
+++ provision-k8s/vars.mk
@@ -5,12 +5,12 @@ OVERLAY := k3s-cluster
 
 # Strimzi Version Number as written in the Github Release page: https://github.com/strimzi/strimzi-kafka-operator/releases
 STRIMZI_VERSION := 0.15.0
-
 JAEGER_OPERATOR_VERSION := v1.15.1
+ECK_VERSION := 1.0.0-rc6
+ISTIO_VERSION := 1.4.3
 
-# Namespace for the Strimzi Kafka Cluster Operator.
+# Namespaces
 STRIMZI_OPERATOR_NS := kafka-operator
 JAEGER_OPERATOR_NS := jaeger-operator
-ECK_VERSION := 1.0.0-rc6
-
-ISTIO_VERSION := 1.4.3
+JAEGER_STACK_NS := jaeger-tracing-stack
+ELASTIC_STACK_NS := elastic-monitoring-stack
