6ad71fabc070c1f4a6f09e081b9c5b4da8f73712
Clément Cassé <casse.clement@outlook.com>
2020-01-17 10:40:31 +0100

    upgrading strimzi to 0.16.0

--- provision-k8s/vars.mk
+++ provision-k8s/vars.mk
@@ -4,7 +4,7 @@ APPLICATION_NS := trace-grapher
 OVERLAY := k3s-cluster
 
 # Strimzi Version Number as written in the Github Release page: https://github.com/strimzi/strimzi-kafka-operator/releases
-STRIMZI_VERSION := 0.15.0
+STRIMZI_VERSION := 0.16.0
 JAEGER_OPERATOR_VERSION := v1.15.1
 ECK_VERSION := 1.0.0-rc6
 ISTIO_VERSION := 1.4.3
