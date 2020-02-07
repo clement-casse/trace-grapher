e9c0b976a3e092cd37772677eea5d6a708777a6e
Clément Cassé <casse.clement@outlook.com>
2020-02-07 17:29:54 +0100

    Working on Opencensus integration adding Kibana to visualize Jaeger Traces adding dependencies to jupyter

--- deploy-trace-grapher/bases/trace-grapher/jupyter-startup-hooks/prepare-env.sh
+++ deploy-trace-grapher/bases/trace-grapher/jupyter-startup-hooks/prepare-env.sh
@@ -4,12 +4,14 @@ set -eux;
 
 # Install extensions
 conda install --yes --quiet --prefix "${CONDA_DIR}" -c conda-forge \
+    ipywidgets \
     jupyter_contrib_nbextensions \
     jupyter_nbextensions_configurator \
+    qgrid \
 ;
 
-python3 -m pip -qq install \
-    RISE \
+conda install --yes --quiet --prefix "${CONDA_DIR}" -c damianavila82 \
+    rise \
 ;
 
 jupyter nbextension enable codefolding/edit;
@@ -18,9 +20,12 @@ jupyter nbextension enable splitcell/splitcell;
 jupyter nbextension enable toc2/main;
 jupyter nbextension enable varInspector/main;
 
+jupyter-nbextension install rise --py --sys-prefix
+jupyter-nbextension enable rise --py --sys-prefix
+
 # Download, convert to Python3 then install libmetric
 git clone https://github.com/cznewt/python-libmetric.git;
 cd python-libmetric;
 2to3 -w libmetric/*;
-python setup.py install;
+python setup.py install --user;
 cd ..;
