c185c4e20a4d993c2330117fcf1a5008b76cb91a
Clément Cassé <casse.clement@outlook.com>
2020-02-11 16:45:29 +0100

    Updating operators

--- provision-k8s/vars.mk
+++ provision-k8s/vars.mk
@@ -1,13 +1,13 @@
 # Strimzi Version Number as written in the Github Release page: https://github.com/strimzi/strimzi-kafka-operator/releases
-STRIMZI_VERSION := 0.16.0
+STRIMZI_VERSION := 0.16.2
 STRIMZI_OPERATOR_NS := strimzi-system
 
 # Jaeger Version Number as written in the GitHub Release page
-JAEGER_OPERATOR_VERSION := v1.15.1
+JAEGER_OPERATOR_VERSION := v1.16.0
 JAEGER_OPERATOR_NS := jaeger-system
 
 # Elastic Cloud Kubernetes Operator Version
-ECK_VERSION := 1.0.0-rc6
+ECK_VERSION := 1.0.1
 
 
 # 
