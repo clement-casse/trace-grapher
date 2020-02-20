626b4a3921c26d1602e592efe3d988ec3de89cbd
Clément Cassé <casse.clement@outlook.com>
2020-02-20 10:22:30 +0100

    Deleting Libmetric prefering own implementation

--- deploy-trace-grapher/bases/jupyter/startup-hooks/prepare-env.sh
+++ deploy-trace-grapher/bases/jupyter/startup-hooks/prepare-env.sh
@@ -14,18 +14,17 @@ conda install --yes --quiet --prefix "${CONDA_DIR}" -c damianavila82 \
     rise \
 ;
 
+jupyter nbextension enable autoscroll/main;
+jupyter nbextension enable code_prettify/code_prettify;
+jupyter nbextension enable codefolding/main;
 jupyter nbextension enable codefolding/edit;
+jupyter nbextension enable collapsible_headings/main;
 jupyter nbextension enable init_cell/main;
+jupyter nbextension enable python-markdown/main;
 jupyter nbextension enable splitcell/splitcell;
 jupyter nbextension enable toc2/main;
 jupyter nbextension enable varInspector/main;
 
+
 jupyter-nbextension install rise --py --sys-prefix
 jupyter-nbextension enable rise --py --sys-prefix
-
-# Download, convert to Python3 then install libmetric
-git clone https://github.com/cznewt/python-libmetric.git;
-cd python-libmetric;
-2to3 -w libmetric/*;
-python setup.py install --user;
-cd ..;
