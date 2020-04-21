5a30798f2455142fa17e7234ce07714547150f87
Clément Cassé <casse.clement@outlook.com>
2020-04-21 12:22:29 +0200

    updating operator versions and install process

--- provision-k8s/vars.mk
+++ provision-k8s/vars.mk
@@ -1,9 +1,9 @@
 # Strimzi Version Number as written in the Github Release page: https://github.com/strimzi/strimzi-kafka-operator/releases
-STRIMZI_VERSION := 0.16.2
+STRIMZI_VERSION := 0.17.0
 STRIMZI_OPERATOR_NS := strimzi-system
 
 # Jaeger Version Number as written in the GitHub Release page
-JAEGER_OPERATOR_VERSION := v1.16.0
+JAEGER_OPERATOR_VERSION := v1.17.1
 JAEGER_OPERATOR_NS := jaeger-system
 
 # Elastic Cloud Kubernetes Operator Version
@@ -12,6 +12,7 @@ ECK_VERSION := 1.0.1
 
 # 
 PROM_OPERATOR_NS := prometheus-system
+PROM_OPERATOR_VERSION := v0.38.1
 
 # Istio Version
 ISTIO_VERSION := 1.4.6
