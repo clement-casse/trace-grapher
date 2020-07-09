cf6f5cebdeeb1dbdceea7d7ce6a3d7a8c8b3c657
Clément Cassé <casse.clement@outlook.com>
2020-07-09 11:28:28 +0200

    Yet another structuration of the project

--- provision-k8s/vars.mk
+++ provision-k8s/vars.mk
@@ -3,7 +3,7 @@ STRIMZI_VERSION := 0.17.0
 STRIMZI_OPERATOR_NS := strimzi-system
 
 # Jaeger Version Number as written in the GitHub Release page
-JAEGER_OPERATOR_VERSION := v1.17.1
+JAEGER_OPERATOR_VERSION := v1.18.1
 JAEGER_OPERATOR_NS := jaeger-system
 
 # Elastic Cloud Kubernetes Operator Version
@@ -15,7 +15,7 @@ PROM_OPERATOR_NS := prometheus-system
 PROM_OPERATOR_VERSION := v0.38.1
 
 # Istio Version
-ISTIO_VERSION := 1.4.6
+ISTIO_VERSION := 1.6.4
 
 
 CERTMANAGER_VERSION := v0.14.2
